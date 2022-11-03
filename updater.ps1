f08b44a97fd6fc888db0b8cde852d0c6d5f880cf
xhico <francgf@gmail.com>
2022-11-03 23:06:51 +0000

    Update updater.ps1

--- updater.ps1
+++ updater.ps1
@@ -4,16 +4,19 @@ $files = @("server.py", "gunicorn_config.py", "HWMonitorServer\__init__.py", "HW
 foreach ($file in $files)
 {
     Copy-Item .\$file Y:\HWMonitorServer\$file -Force
+    Copy-Item .\$file X:\HWMonitorServer\$file -Force
 }
-
 
 $folders = @("main", "stats", "bots", "history", "eye", "static\css", "static\images", "static\js")
 foreach ($folder in $folders)
 {
     Remove-Item Y:\HWMonitorServer\HWMonitorServer\$folder\* -R -Force
     Copy-Item .\HWMonitorServer\$folder\* Y:\HWMonitorServer\HWMonitorServer\$folder\ -R -Force
+
+    Remove-Item X:\HWMonitorServer\HWMonitorServer\$folder\* -R -Force
+    Copy-Item .\HWMonitorServer\$folder\* X:\HWMonitorServer\HWMonitorServer\$folder\ -R -Force
 }
 
 Invoke-WebRequest -UseBasicParsing "http://192.168.1.14:33377/main/power" -Method POST -Body "{ 'option':'restart'}"
-#Invoke-WebRequest -UseBasicParsing "http://192.168.1.15:33377/main/power" -Method POST -Body "{ 'option':'restart'}"
+Invoke-WebRequest -UseBasicParsing "http://192.168.1.15:33377/main/power" -Method POST -Body "{ 'option':'restart'}"
 

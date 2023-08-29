f8b80b8c35ce7e9a97c6043da04a18ac84588667
xhico <francgf@gmail.com>
2023-08-29 17:25:55 +0100

    Dynamic Apache Conf File

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,11 +3,27 @@
 sudo apt install python3 python3-pip python3-setuptools python3-venv -y
 python3 -m pip install flask gunicorn requests gpiozero psutil --no-cache-dir
 
-sudo mv /home/pi/HWMonitorServer/HWMonitorServer.conf /etc/apache2/sites-available/
+echo """
+<VirtualHost *:80>
+    ServerName monitor.$(hostname).xhico
+    ErrorLog ${APACHE_LOG_DIR}/HWMonitorServer-error.log
+    CustomLog ${APACHE_LOG_DIR}/HWMonitorServer-access.log combined
+
+    <Proxy *>
+        Order deny,allow
+        Allow from all
+    </Proxy>
+    ProxyPreserveHost On
+
+    <Location "/">
+        ProxyPass unix:/run/HWMonitorServer.sock|http://127.0.0.1/
+        ProxyPassReverse unix:/run/HWMonitorServer.sock|http://127.0.0.1/
+    </Location>
+</VirtualHost>
+"""  | sudo tee -a /etc/apache2/sites-available/HWMonitorServer.conf
+
 sudo mv /home/pi/HWMonitorServer/HWMonitorServer.socket /etc/systemd/system/
 sudo mv /home/pi/HWMonitorServer/HWMonitorServer.service /etc/systemd/system/
-
-echo "Listen 33377" | sudo tee -a /etc/apache2/ports.conf
 
 sudo a2ensite HWMonitorServer.conf
 sudo systemctl enable apache2

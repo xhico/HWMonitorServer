667a46592aff9ea6c7a055cbe8fc94f1984eaddf
xhico <francgf@gmail.com>
2023-08-30 11:45:01 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,9 +2,14 @@
 
 sudo apt install python3 python3-pip python3-setuptools python3-venv -y
 python3 -m pip install flask gunicorn requests gpiozero psutil --no-cache-dir
+sudo apt install certbot -y
+mkdir -p /home/pi/certs/
+openssl genrsa -out /home/pi/certs/HWMonitorServer.key 2048
+openssl req -new -key /home/pi/certs/HWMonitorServer.key -out /home/pi/certs/HWMonitorServer.csr -subj "/C=PT/ST=Leiria/L=Óbidos/O=xhico/CN=HWMonitorServer@$(hostname)"
+openssl x509 -req -days 365 -in /home/pi/certs/HWMonitorServer.csr -signkey /home/pi/certs/HWMonitorServer.key -out /home/pi/certs/HWMonitorServer.crt
 
 echo """
-<VirtualHost *:80>
+<VirtualHost *:443>
     ServerName monitor.$(hostname).xhico
     ErrorLog ${APACHE_LOG_DIR}/HWMonitorServer-error.log
     CustomLog ${APACHE_LOG_DIR}/HWMonitorServer-access.log combined
@@ -19,6 +24,10 @@ echo """
         ProxyPass unix:/run/HWMonitorServer.sock|http://127.0.0.1/
         ProxyPassReverse unix:/run/HWMonitorServer.sock|http://127.0.0.1/
     </Location>
+
+    SSLEngine on
+    SSLCertificateFile /home/pi/certs/HWMonitorServer.crt
+    SSLCertificateKeyFile /home/pi/certs/HWMonitorServer.key
 </VirtualHost>
 """  | sudo tee -a /etc/apache2/sites-available/HWMonitorServer.conf
 
